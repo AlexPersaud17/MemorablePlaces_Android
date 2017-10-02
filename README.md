5b6def9c565823341d7587f0fac55cc73cbe5fb1
Alex Persaud <alex_persaud01@yahoo.com>
2017-10-02 14:42:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# The next project on my Android Development journey!
+## This is an app that uses the Google Maps API to allow users to save memorable places that they have been to by long pressing on the map!
---
+++ app/src/main/java/com/alexpersaud/memorableplaces/MainActivity.java
@@ -0,0 +1,45 @@
+package com.alexpersaud.memorableplaces;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    static ArrayList<String> places;
+    static ArrayAdapter arrayAdapter;
+    static ArrayList<LatLng> locations;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+        places = new ArrayList<>();
+        places.add("Add a new place...");
+
+        locations = new ArrayList<>();
+        locations.add(new LatLng(0, 0));
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, places);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                intent.putExtra("placeNumber", position);
+                startActivity(intent);
+            }
+        });
+    }
+}
